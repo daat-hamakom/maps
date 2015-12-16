112d59e15ace6e4a6bba63aedf7c672387e3d7d3
Yuval Adam <yuval@y3xz.com>
2015-12-16 13:29:18 +0200

    Add admin redirect

--- server.js
+++ server.js
@@ -8,14 +8,18 @@ const app = express()
 const production = (process.env['NODE_ENV'] === 'production')
 const static_path = path.join(process.cwd(), 'static')
 
+
 app.use(compress())
 app.disable('x-powered-by')
 
 app.use('/static', express.static(static_path))
+
 app.get('/', function (req, res) {
-  res.sendFile('index.html', {
-    root: process.cwd()
-  });
+  res.sendFile('index.html', { root: process.cwd() })
+})
+
+app.get('/admin', function (req, res) {
+  res.redirect('https://daat-hamakom-data.herokuapp.com/admin/')
 })
 
 const port = Number(process.env['PORT'] || 3000)
@@ -24,12 +28,15 @@ app.listen(port, function () {
   console.log(`✅ Server running on port ${port}`)
 })
 
+
 if (!production) {
   var config = require('./webpack.config');
   var WebpackDevServer = require('webpack-dev-server');
 
-  new WebpackDevServer(webpack(config)).listen(3000, 'localhost', function (err, result) {
-    if (err) { console.log(err) }
-    console.log('Listening at localhost:3000');
-  });
+  new WebpackDevServer(webpack(config)).listen(3000, 'localhost', function (err, res) {
+    if (err) {
+      console.log(err)
+    }
+    console.log('Listening at localhost:3000')
+  })
 }

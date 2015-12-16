031d0163780a79f2798e70e73b6caf7fa6ec6a40
Yuval Adam <yuval@y3xz.com>
2015-12-16 13:06:33 +0200

    Basic express dev/prod setup working

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import path from 'path'
+import webpack from 'webpack'
+
+const app = express()
+
+const production = (process.env['NODE_ENV'] === 'production')
+const static_path = path.join(process.cwd(), 'static')
+
+app.use('/static', express.static(static_path))
+app.get('/', function (req, res) {
+  res.sendFile('index.html', {
+    root: process.cwd()
+  });
+})
+
+const port = Number(process.env['PORT'] || 3000)
+
+app.listen(port, function () {
+  console.log(`✅  ==> server running at http://localhost:${port}`)
+})
+
+if (!production) {
+  var config = require('./webpack.config');
+  var WebpackDevServer = require('webpack-dev-server');
+
+  new WebpackDevServer(webpack(config)).listen(3000, 'localhost', function (err, result) {
+    if (err) { console.log(err) }
+    console.log('Listening at localhost:3000');
+  });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: './index.js',
     output: {
         path: __dirname,
-        filename: 'bundle.js',
+        filename: 'static/js/bundle.js',
     },
     module: {
         loaders: [

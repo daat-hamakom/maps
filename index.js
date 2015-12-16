8e0124bfa1b0c9a0fb0cb8936fecd7735e28fb62
Yuval Adam <yuval@y3xz.com>
2015-12-16 11:56:35 +0200

    Reactive routing almost working

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ import './style.scss'
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
+import { Router, Route } from 'react-router'
+import About from './src/components/about';
+import App from './src/components/app';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render((
+  <Router>
+    <Route path="/" component={App}>
+      <Route path="about" component={About}/>
+    </Route>
+  </Router>
+), document.getElementById('root'))

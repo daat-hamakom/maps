6c73b391b3902fa562c8c37ee91888cd4dcbdc8f
Yuval Adam <yuval@y3xz.com>
2016-01-17 14:54:51 +0200

    YearAxis implemented

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -2,6 +2,68 @@ import d3 from 'd3'
 import React from 'react'
 import ReactFauxDOM from 'react-faux-dom'
 
+class YearAxis extends React.Component {
+
+  static propTypes = {
+    width: React.PropTypes.number,
+    height: React.PropTypes.number
+  }
+
+  componentWillMount () {
+    const {width, height} = this.props
+
+    this.x = d3.time.scale()
+        .domain([new Date(1950, 1, 1), new Date(2000, 1, 1)])
+        .nice(d3.time.year)
+        .range([0, width]);
+
+    this.xAxis = d3.svg.axis()
+        .scale(this.x)
+        .orient('bottom')
+        .ticks(d3.time.years, 5)
+        .tickSize(height)
+  }
+
+  componentDidMount () {
+    this.renderAxis()
+  }
+
+  componentDidUpdate () {
+    this.renderAxis()
+  }
+
+  renderAxis () {
+
+    // el.selectAll('rect').data(data).enter()
+    //     .append('rect')
+    //     .attr('class', 'rect')
+    //     .attr('x', function (d,i) { return x(new Date(d[0], 1, 1)) })
+    //     .attr('width', function (d,i) { return x(new Date(d[1], 1, 1)) - x(new Date(d[0], 1, 1)) })
+    //     .attr('y', function (d,i) { return 40+(i*10) })
+    //     .attr('height', function (d,i) { return 5 })
+
+    const yearAxisG = d3.select(this.refs.yearAxis).call(this.xAxis)
+
+    yearAxisG.selectAll('.tick text')
+        .style('text-anchor', 'start')
+        .attr('x', 6)
+        .attr('y', 6)
+
+    yearAxisG.selectAll('line').data(this.x.ticks(150), function(d) { return d; })
+        .enter()
+        .append('line')
+        .attr('class', 'minor')
+        .attr('y1', 22)
+        .attr('y2', this.props.height)
+        .attr('x1', this.x)
+        .attr('x2', this.x)
+  }
+
+  render () {
+    return <g ref='yearAxis' className='x axis' />
+  }
+}
+
 class D3Timeline extends React.Component {
   static propTypes = {
     width: React.PropTypes.number,
@@ -10,50 +72,9 @@ class D3Timeline extends React.Component {
   }
 
   render () {
-    const {width, height, data, interpolation} = this.props
-
-    const el = d3.select(ReactFauxDOM.createElement('svg'))
-      .attr(this.props)
-      .attr('data', null)
-
-    const x = d3.time.scale()
-        .domain([new Date(1950, 1, 1), new Date(2000, 1, 1)])
-        .nice(d3.time.year)
-        .range([0, width]);
-
-    const xAxis = d3.svg.axis()
-        .scale(x)
-        .orient('bottom')
-        .ticks(d3.time.years, 5)
-        .tickSize(height)
-
-    const xAxisG = el.append('g')
-        .attr('class', 'x axis')
-        .call(xAxis)
-
-    xAxisG.selectAll('.tick text')
-        .style('text-anchor', 'start')
-        .attr('x', 6)
-        .attr('y', 6)
-
-    xAxisG.selectAll("line").data(x.ticks(150), function(d) { return d; })
-        .enter()
-        .append("line")
-        .attr("class", "minor")
-        .attr("y1", 22)
-        .attr("y2", height)
-        .attr("x1", x)
-        .attr("x2", x)
-
-    el.selectAll('rect').data(data).enter()
-        .append('rect')
-        .attr('class', 'rect')
-        .attr('x', function (d,i) { return x(new Date(d[0], 1, 1)) })
-        .attr('width', function (d,i) { return x(new Date(d[1], 1, 1)) - x(new Date(d[0], 1, 1)) })
-        .attr('y', function (d,i) { return 40+(i*10) })
-        .attr('height', function (d,i) { return 5 })
-
-    return el.node().toReact()
+    return <svg width={this.props.width} height={this.props.height}>
+      <YearAxis width={this.props.width} height={this.props.height} />
+    </svg>
   }
 }
 
@@ -68,11 +89,7 @@ class Timeline extends React.Component {
     ]
 
     return <div id='timeline'>
-      <D3Timeline
-        className='example'
-        width={document.body.offsetWidth}
-        height={200}
-        data={data} />
+      <D3Timeline width={document.body.offsetWidth} height={200} data={data} />
     </div>
   }
 }

d2dc8cc8a425101b685a535e61878d80a6d1d506
Yuval Adam <yuval@y3xz.com>
2016-01-17 17:01:39 +0200

    We have working markers

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -9,19 +9,13 @@ class YearAxis extends React.Component {
     height: React.PropTypes.number
   }
 
-  componentWillMount () {
-    const {width, height} = this.props
-
-    this.x = d3.time.scale()
-        .domain([new Date(1950, 1, 1), new Date(2000, 1, 1)])
-        .nice(d3.time.year)
-        .range([0, width]);
-
+  constructor (props) {
+    super(props)
     this.xAxis = d3.svg.axis()
-        .scale(this.x)
-        .orient('bottom')
-        .ticks(d3.time.years, 5)
-        .tickSize(height)
+      .scale(props.x)
+      .orient('bottom')
+      .ticks(d3.time.years, 5)
+      .tickSize(props.height)
   }
 
   componentDidMount () {
@@ -34,14 +28,6 @@ class YearAxis extends React.Component {
 
   renderAxis () {
 
-    // el.selectAll('rect').data(data).enter()
-    //     .append('rect')
-    //     .attr('class', 'rect')
-    //     .attr('x', function (d,i) { return x(new Date(d[0], 1, 1)) })
-    //     .attr('width', function (d,i) { return x(new Date(d[1], 1, 1)) - x(new Date(d[0], 1, 1)) })
-    //     .attr('y', function (d,i) { return 40+(i*10) })
-    //     .attr('height', function (d,i) { return 5 })
-
     const yearAxisG = d3.select(this.refs.yearAxis).call(this.xAxis)
 
     yearAxisG.selectAll('.tick text')
@@ -49,18 +35,41 @@ class YearAxis extends React.Component {
         .attr('x', 6)
         .attr('y', 6)
 
-    yearAxisG.selectAll('line').data(this.x.ticks(150), function(d) { return d; })
+    yearAxisG.selectAll('line').data(this.props.x.ticks(150), function(d) { return d; })
         .enter()
         .append('line')
         .attr('class', 'minor')
         .attr('y1', 22)
         .attr('y2', this.props.height)
-        .attr('x1', this.x)
-        .attr('x2', this.x)
+        .attr('x1', this.props.x)
+        .attr('x2', this.props.x)
   }
 
   render () {
     return <g ref='yearAxis' className='x axis' />
+  }
+}
+
+class MarkerData extends React.Component {
+  static propTypes = {
+    x: React.PropTypes.func,
+    data: React.PropTypes.array,
+  }
+
+  render () {
+    return <g ref='markerData' className='markers'>
+      {this.props.data.map((d) => {
+        const markerProps = {
+          x: this.props.x(new Date(d[0], 1, 1)),
+          width: this.props.x(new Date(d[1], 1, 1)) - this.props.x(new Date(d[0], 1, 1)),
+          y: 40+(d[2]*10),
+          height: 5,
+          key: d[2],
+          className: 'marker'
+        }
+        return <rect {...markerProps}></rect>
+      })}
+    </g>
   }
 }
 
@@ -71,9 +80,18 @@ class D3Timeline extends React.Component {
     data: React.PropTypes.array,
   }
 
+  constructor (props) {
+    super(props)
+    this.x = d3.time.scale()
+      .domain([new Date(1950, 1, 1), new Date(2000, 1, 1)])
+      .nice(d3.time.year)
+      .range([0, this.props.width])
+  }
+
   render () {
     return <svg width={this.props.width} height={this.props.height}>
-      <YearAxis width={this.props.width} height={this.props.height} />
+      <YearAxis x={this.x} width={this.props.width} height={this.props.height} />
+      <MarkerData x={this.x} data={this.props.data} />
     </svg>
   }
 }
@@ -81,11 +99,11 @@ class D3Timeline extends React.Component {
 class Timeline extends React.Component {
   render () {
     const data = [
-      [1961, 1969],
-      [1964, 1967],
-      [1971, 1999],
-      [1955, 1972],
-      [1984, 1996]
+      [1961, 1969, 0],
+      [1964, 1967, 1],
+      [1971, 1999, 2],
+      [1955, 1972, 3],
+      [1984, 1996, 4]
     ]
 
     return <div id='timeline'>

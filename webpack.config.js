8e53acc4469c08d72255bb405835f1a19fc1b768
Yuval Adam <yuval@y3xz.com>
2016-02-04 12:25:00 +0200

    Fix fetch polyfill on old browsers

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var bourbon = require('node-bourbon').includePaths
+var webpack = require('webpack')
 
 module.exports = {
     entry: './index.js',
@@ -11,5 +12,10 @@ module.exports = {
             { test: /\.js$/, exclude: /node_modules/, loader: 'babel' },
             { test: /\.scss$/, loader: 'style!css!sass?includePaths[]=' + bourbon }
         ]
-    }
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+        })
+    ]
 }

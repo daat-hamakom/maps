8901dc68e34d8a3c0058090f406fdd9e15ed21b8
Yuval Adam <yuval@y3xz.com>
2016-02-29 15:31:40 +0200

    Refactor out initMap()

--- src/components/map.js
+++ src/components/map.js
@@ -30,141 +30,144 @@ class GLMap extends React.Component {
     this.markers = false
   }
 
+  initMap() {
+    this.markers = true
+
+    const markerData = {
+      'type': 'FeatureCollection',
+      'features': this.props.events.items.map((ev, index) => {
+        return {
+          'type': 'Feature',
+          'properties': {
+            'description': ev.title,
+            'evid': ev.id,
+            'icon': ev.icon,
+            'marker-symbol': 'default-marker'
+          },
+          'geometry': {
+            'type': 'Point',
+            'coordinates': ev.place.position.split(',').map(x => +x).reverse()
+          }
+        }
+      })
+    }
+
+    var sourceObj = new mapboxgl.GeoJSONSource({
+      'data': markerData,
+      'cluster': true,
+      'clusterMaxZoom': 14
+    })
+
+    this.map.addSource('eventMarkers', sourceObj)
+
+    this.map.addLayer({
+      'id': 'markers',
+      'type': 'symbol',
+      'source': 'eventMarkers',
+      'interactive': true,
+      'layout': {
+        'icon-image': '{marker-symbol}',
+        'icon-allow-overlap': true
+      }
+    })
+
+    this.map.addLayer({
+      'id': 'clusters',
+      'type': 'circle',
+      'source': 'eventMarkers',
+      'paint': {
+        'circle-color': '#2dc6e0',
+        'circle-opacity': 0.85,
+        'circle-radius': 10
+      },
+      'filter': ['>', 'point_count', 1]
+    });
+
+    this.map.addLayer({
+      'id': 'cluster-count',
+      'type': 'symbol',
+      'source': 'eventMarkers',
+      'layout': {
+        'text-field': '{point_count}',
+        'text-font': [
+          'DIN Offc Pro Medium',
+          'Arial Unicode MS Bold'
+        ],
+        'text-size': 11
+      },
+      'paint': {
+        'text-color': '#ffffff'
+      }
+    });
+
+    this.hover_popup = new mapboxgl.Popup({
+      closeButton: false,
+      closeOnClick: false,
+      anchor: 'top'
+    })
+
+    this.select_popup = new mapboxgl.Popup({
+      closeButton: false,
+      closeOnClick: false,
+      anchor: 'top'
+    })
+
+    this.hovering = false
+    this.selecting = false
+
+    this.map.on('mousemove', (e) => {
+      this.map.featuresAt(e.point, {layer: 'markers', radius: 7, includeGeometry: true}, (err, features) => {
+        if (err || !features.length) {
+          if (this.hovering) {
+            this.hover_popup.remove()
+            this.props.hoverExitEvent()
+            this.hovering = false
+          }
+          return
+        }
+
+        if (!this.hovering) {
+          this.hovering = true
+          this.hover_popup.setLngLat(features[0].geometry.coordinates)
+            .setHTML('<div class="marker-popup">' +
+              '<div class="icon"><img src="' + features[0].properties.icon + '"></div>' +
+              '<div class="connector"></div>' +
+              '<div class="dot"></div>' +
+              '</div>')
+            .addTo(this.map)
+
+          // fugly hack to grab the click on the popup
+          this.hover_popup._content.onclick = (_e) => {
+            const featureIds = features.map((e) => e.properties.evid)
+            this.props.openEventSidepane(this.props.events.items.filter(
+              (ev) => featureIds.includes(ev.id)
+            ))
+          }
+
+          this.props.hoverEnterEvent(features[0].properties.evid)
+        }
+      })
+    })
+
+    this.map.on('click', (e) => {
+      this.map.featuresAt(e.point, {
+        layer: 'markers', radius: 10, includeGeometry: true
+      }, (err, features) => {
+        if (err || !features.length)
+          return;
+
+        const featureIds = features.map((e) => e.properties.evid)
+        this.props.openEventSidepane(this.props.events.items.filter(
+          (ev) => featureIds.includes(ev.id)
+        ))
+
+      })
+    })
+  }
+
   componentDidUpdate () {
     if (!this.props.events.fetching && !this.markers && this.props.events.items.length > 0) {
-
-      this.markers = true
-
-      const markerData = {
-        'type': 'FeatureCollection',
-        'features': this.props.events.items.map((ev, index) => {
-          return {
-            'type': 'Feature',
-            'properties': {
-              'description': ev.title,
-              'evid': ev.id,
-              'icon': ev.icon,
-              'marker-symbol': 'default-marker'
-            },
-            'geometry': {
-              'type': 'Point',
-              'coordinates': ev.place.position.split(',').map(x => +x).reverse()
-            }
-          }
-        })
-      }
-
-      var sourceObj = new mapboxgl.GeoJSONSource({
-        'data': markerData,
-        'cluster': true,
-        'clusterMaxZoom': 14
-      })
-
-      this.map.addSource('eventMarkers', sourceObj)
-
-      this.map.addLayer({
-        'id': 'markers',
-        'type': 'symbol',
-        'source': 'eventMarkers',
-        'interactive': true,
-        'layout': {
-          'icon-image': '{marker-symbol}',
-          'icon-allow-overlap': true
-        }
-      })
-
-      this.map.addLayer({
-        'id': 'clusters',
-        'type': 'circle',
-        'source': 'eventMarkers',
-        'paint': {
-          'circle-color': '#2dc6e0',
-          'circle-opacity': 0.85,
-          'circle-radius': 10
-        },
-        'filter': ['>', 'point_count', 1]
-      });
-
-      this.map.addLayer({
-        'id': 'cluster-count',
-        'type': 'symbol',
-        'source': 'eventMarkers',
-        'layout': {
-          'text-field': '{point_count}',
-          'text-font': [
-            'DIN Offc Pro Medium',
-            'Arial Unicode MS Bold'
-          ],
-          'text-size': 11
-        },
-        'paint': {
-          'text-color': '#ffffff'
-        }
-      });
-
-      this.hover_popup = new mapboxgl.Popup({
-        closeButton: false,
-        closeOnClick: false,
-        anchor: 'top'
-      })
-
-      this.select_popup = new mapboxgl.Popup({
-        closeButton: false,
-        closeOnClick: false,
-        anchor: 'top'
-      })
-
-      this.hovering = false
-      this.selecting = false
-
-      this.map.on('mousemove', (e) => {
-        this.map.featuresAt(e.point, {layer: 'markers', radius: 7, includeGeometry: true}, (err, features) => {
-          if (err || !features.length) {
-            if (this.hovering) {
-              this.hover_popup.remove()
-              this.props.hoverExitEvent()
-              this.hovering = false
-            }
-            return
-          }
-
-          if (!this.hovering) {
-            this.hovering = true
-            this.hover_popup.setLngLat(features[0].geometry.coordinates)
-              .setHTML('<div class="marker-popup">' +
-                '<div class="icon"><img src="' + features[0].properties.icon + '"></div>' +
-                '<div class="connector"></div>' +
-                '<div class="dot"></div>' +
-                '</div>')
-              .addTo(this.map)
-
-            // fugly hack to grab the click on the popup
-            this.hover_popup._content.onclick = (_e) => {
-              const featureIds = features.map((e) => e.properties.evid)
-              this.props.openEventSidepane(this.props.events.items.filter(
-                (ev) => featureIds.includes(ev.id)
-              ))
-            }
-
-            this.props.hoverEnterEvent(features[0].properties.evid)
-          }
-        })
-      })
-
-      this.map.on('click', (e) => {
-        this.map.featuresAt(e.point, {
-          layer: 'markers', radius: 10, includeGeometry: true
-        }, (err, features) => {
-          if (err || !features.length)
-            return;
-
-          const featureIds = features.map((e) => e.properties.evid)
-          this.props.openEventSidepane(this.props.events.items.filter(
-            (ev) => featureIds.includes(ev.id)
-          ))
-
-        })
-      })
+      this.initMap()
     }
   }
 

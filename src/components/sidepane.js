9426d6df522fc04ce7a9808aa6219c2bd34e948f
Yuval Adam <yuval@y3xz.com>
2016-03-14 15:09:55 +0200

    Add audio media items elements

--- src/components/sidepane.js
+++ src/components/sidepane.js
@@ -54,6 +54,9 @@ class EventPane extends React.Component {
         <div className='place'>{ev.place.name}</div>
         <div className='date'>{this._cleanDates(ev.start_date, ev.end_date)}</div>
         <hr/>
+        {ev.media.filter((m) => m.type == 'sound').map((m) => <audio controls>
+          <source src={m.file} type="audio/mpeg"/>
+        </audio>)}
         <div className='description' dangerouslySetInnerHTML={{__html: ev.description}}></div>
       </div>
     </div>

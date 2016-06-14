d77b980364b063a3a3ae041b15e00e993e18a0a9
Yuval Adam <yuval@y3xz.com>
2016-06-14 16:54:22 +0300

    Bump minor mapbox version

--- webpack.config.js
+++ webpack.config.js
@@ -21,12 +21,22 @@ module.exports = {
                 include: path.resolve(__dirname, 'node_modules/mapbox-gl/js/render/painter/use_program.js'),
                 loader: 'transform/cacheable?brfs'
             },
+            {
+                test: /\.js$/,
+                include: path.resolve(__dirname, 'node_modules/mapbox-gl-shaders/index.js'),
+                loader: 'transform/cacheable?brfs'
+            },
             { test: /\.js$/, exclude: /node_modules/, loader: 'babel' },
             { test: /\.scss$/, loader: 'style!css!sass?includePaths[]=' + bourbon }
         ]
     },
     postLoaders: [{
         include: /node_modules\/mapbox-gl/,
+        loader: 'transform',
+        query: 'brfs'
+    },
+    {
+        include: /node_modules\/mapbox-gl-shaders/,
         loader: 'transform',
         query: 'brfs'
     }],

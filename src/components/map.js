13aa36f5781b4c1340f776043a267b669bc14e54
Mushon Zer-Aviv <mushon@shual.com>
2016-06-28 15:47:59 +0300

    new modernism map

--- src/components/map.js
+++ src/components/map.js
@@ -21,9 +21,9 @@ function getEventStyle (ev) {
   if (ev.start_date < '1789-')  // vive la revolution
     return 'preindustrial'
   if (ev.start_date < '1900-')  // vive la revolution
-    return 'default'            // until we have the Industrial map
+    return 'modernism'            // until we have the Industrial map
   if (ev.start_date < '1939-')  // vive la revolution
-    return 'default'            // until we have the Modernism map
+    return 'modernism'            // until we have the Modernism map
   if (ev.start_date < '1945-')  // vive la revolution
     return 'darktimes'
   if (ev.start_date < '2050-')  // vive la revolution

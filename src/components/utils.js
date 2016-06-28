b0a0dc968e8bbec8c595e3b333cef25725703433
Yuval Adam <yuval@y3xz.com>
2016-06-28 23:35:53 +0300

    Call audio element load() to force src change, fixes #136

--- src/components/utils.js
+++ src/components/utils.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 class AudioComponent extends React.Component {
 
+  componentDidUpdate (prevProps) {
+    if (this.props.src != prevProps.src) {
+      const node = this.refs.audio
+      node.load()
+    }
+  }
+
   render () {
-    return <audio controls>
+    return <audio ref='audio' controls>
       <source src={this.props.src} type={this.props.type}/>
     </audio>
   }

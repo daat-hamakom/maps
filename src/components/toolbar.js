81ea0f2e91f929dab7179f38629a1e29ebae923f
Tal <tal@talbenbasat.com>
2017-08-06 11:49:24 +0300

    Fixed help and about

--- src/components/toolbar.js
+++ src/components/toolbar.js
@@ -45,10 +45,10 @@ export class AboutModal extends Component {
           <div>
             <Nav>
               <NavItem eventKey="about-map" className="modal-tab-nav">
-                About Jewish Cultures on the Map
+                About Jewish Cultures Mapped
               </NavItem>
               <NavItem eventKey="about-daat" className="modal-tab-nav">
-                About Daat-Hamakom
+                About Da'at-Hamakom
               </NavItem>
               <NavItem eventKey="about-data" className="modal-tab-nav">
                 About the Data
@@ -62,35 +62,110 @@ export class AboutModal extends Component {
             </Nav>
             <Tab.Content animation>
               <Tab.Pane eventKey="about-map">
-                <h4 className="modal-subtitle">About Jewish Cultures on the Map</h4>
+                <h4 className="modal-subtitle">About Jewish Cultures Mapped</h4>
                 <p>
-                  Jewish Cultures on the Map (JCM) is an interactive web-based map developed by Daat-Hamakom. It is based on innovative digital-mapping and information visualization technologies designed to explore and experience Jewish cultures in their historical development from a perspective of time and space. It is designed as a flexible platform for incorporating, presenting and sharing data from existing databases and archives.
+                  Jewish Cultures Mapped (JCM) is an interactive, web-based map developed by <a href="http://www.daat-hamakom.com/" target="_blank">“Daat Hamakom”</a>,
+                  The Center for the Study of Cultures of Place in Jewish Modernity (I-CORE). The map harnesses
+                  cutting-edge digital-mapping and information-visualization technologies designed to explore and
+                  experience Jewish cultures in their historical development from a perspective of time and space.
+                  JCM serves as a flexible platform for incorporating, presenting and sharing data from existing
+                  databases and archives
                 </p>
                 <p>
-                  The map visualizes dynamic relations and trajectories characteristic of the Jewish world through a uniquely designed interface combining a map with a timeline. It presents multimedia documentary information,including sound,images, and film, along with quantitative geographic information (GIS).
+                  Jewish Cultures Mapped visualizes dynamic relations and trajectories in Jewish culture through a
+                  unique interface, consisting of an integrated map and timeline. It presents multimedia documentary
+                  information, including sound, still images, and video
                 </p>
                 <p>
-                  The map is intended to serve as a context for worldwide scholars and experts of all cultural fields to generate and share time-space based information. JCM provides Easy accessibility of high quality content to a wide range of publics, such as university researchers, schoolteachers, students and laypersons searching for information in a platform that differs from extant searching and data mining engines.
+                  JCM functions as a platform for scholars worldwide, enabling experts in all cultural fields to generate
+                  and share time- and space-based information. It provides easy access to high-quality content
+                  for a wide range of publics, such as university researchers, teachers, students and laypersons in a
+                  platform that differs significantly from existing search and data-mining engines.
+                </p>
+                <p>
+                  <a href="https://www.youtube.com/watch?v=FMScIoF-9kE&t=136s" target="_blank">Watch a short lecture</a>
                 </p>
               </Tab.Pane>
               <Tab.Pane eventKey="about-daat">
-                <h4 className="modal-subtitle">About Daat-Hamakom</h4>
-                <p>The I-CORE's approach to Modern Jewish Culture is the spatial dimension in all of its connotations. We view the geography of culture as a means of “mapping” where and how certain modes of creativity and their traditions became rooted, or – in many cases – when and how they became uprooted and then were transported to new contexts and transformed. The turn to spatial and geographic concerns in the study of the Humanities can be fruitfully and imaginatively applied to the Jewish case and that, in turn, can enrich Humanistic discourse from our particular angle of vision. The idea of “place” anchors our project in a feasible, structured research agenda, relating to the overall question: how do realia of particular places influence behavior, consciousness, beliefs, and creativity?</p>
+                <h4 className="modal-subtitle">About Da'at-Hamakom</h4>
+                <p>
+                  By assembling a forum of eleven senior scholars from four Israeli universities devoted to a variety
+                  of disciplines and regional specializations, “Da’at Hamakom”: The Center for the Study of Cultures
+                  of Place in Jewish Modernity aims to establish a new benchmark in Jewish cultural inquiry. Our
+                  scholars are world-renowned experts in musicology, history, literature, cultural studies, communica-
+                  tion studies, Jewish law (halakha), Jewish thought, theology, art history, material and popular cul-
+                  ture; specialized foci on francophone, Hispanic, English, German, Hebrew, Yiddish, and Arabic cul-
+                  tures; and a comparative approach to cultures considered to be “Western” and “Eastern.”
+                </p>
+                <p>
+                  Daat Hamakom approaches modern Jewish culture via the spatial dimension in its full range of
+                  significations. We view the geography of culture as a means of “mapping” where and how certain
+                  modes of creativity and their traditions emerged, and – in many cases – when and how they were
+                  translocated and transformed. The turn to spatial and geographic concerns in the study of the Hu-
+                  manities can be fruitfully and imaginatively applied to the Jewish case, the probing of which, in
+                  turn, can enrich Humanistic discourse. The idea of “place” anchors our project in a feasible, struc-
+                  tured research agenda, relating to the overall question: How do realia of particular places influence
+                  behavior, consciousness, beliefs, and creativity?
+                </p>
               </Tab.Pane>
               <Tab.Pane eventKey="about-data">
                 <h4 className="modal-subtitle">About the Data</h4>
-                <p>The contents of the map was produced and uploaded by the researchers of Da’at Hamakom and collaborating archives and scholars. The data is arranged as units of information and media called “events” which are accessible through the map and the timeline. Each event is created within an enclosing project, and the creators of the data of each project are listed in the project cards. The source and copyrights owner of the media items within the events is specified within the item display. Additional meta data contain information about historical figures and organizations.</p>
-                <p>Contributing archives: The National Library of Isreal, The Hoffamn Collection of Postcards.</p>
+                <p>
+                  The contents of the map are produced and uploaded by researchers of Da’at Hamakom as well as
+                  drawn from archives of institutions collaborating with this initiative. The data is arranged as units of
+                  information and media called “events”, which are accessible through the map and the timeline.
+                  Each event is found within a “project,” and the creators of the data of each project are listed on the
+                  project cards. The source and copyrights owner of the media items within the events is specified
+                  within the item display. Additional meta data contain information about historical figures and organizations.
+                </p>
+                <p>
+                  Contributing Institutions: <a href="http://web.nli.org.il/sites/NLI/english/Pages/default.aspx" target="_blank">The National Library of Israel</a>,
+                  <a href="http://folklore.huji.ac.il/book/%D7%9E%D7%A8%D7%9B%D7%96%D7%99-%D7%9E%D7%97%D7%A7%D7%A8" target="_blank">The Hoffmann Judaica Postcard Collection</a>
+                  at the Center for Jewish Folklore, The Hebrew University of Jerusalem
+                </p>
               </Tab.Pane>
               <Tab.Pane eventKey="about-copyrights">
                 <h4 className="modal-subtitle">Copyrights Disclaimer</h4>
-                <p>This database was prepared by the Center for the Study of Cultures in the Jewish World in the Modern Age (hereinafter: “Daat-Hamakom”). The foundation of the program is the Israeli Centers for Research Excellence (I-CORE), an initiative designed by the Planning and Budgeting Committee (PBC) and the Government of Israel.</p>
-                <p>The database is designed to map Jewish culture in modern times and is intended for study, research, review and criticism.</p>
-                <p>The information in the database is presented as is. “Daat-Hamakom” shall not be held liable for any damage that may be caused to anyone relying on this information, even if there is a mistake in the information, catalog or any other detail in the database.</p>
-                <p>The contents of the database for which the original copyright holders are not specified, or otherwise stated, belong to the “Daat-Hamakom”. Any other use permitted by law, including for purposes of fair use as defined in the Law, may be performed in this content. The license to use such content is conditional upon the designation of the “Daat-Hamakom” center or the name of the author appearing next to the displayed content while respecting the moral rights of the content creators.</p>
-                <p>The right to use the contents of the database in respect of which the original copyright holders or authors are specified is according to the terms of use of the original owners of the content. “Daat-Hamakom” shall not bear any liability whatsoever as may be made by any person who violates the terms of use of the original rights holders.</p>
-                <p>“Daat-Hamakom” did its best to locate the rights holders in the works in the database and to obtain their consent to the use made in this database, to the extent required. However, there may be cases where, for various reasons, the right holder has not been located despite the tracing efforts made. If you are the owner of a work that was used in the database and you did not give your consent, please contact us at <a href="mailto:josefspr@hotmail.com" target="_top">josefspr@hotmail.com</a> and we will take care of the matter.</p>
-                <p>If you are harmed in any way by information in the database, or if you understand that any material in the database is used, which is not protected under the fair use protection as defined in the law, please contact <a href="mailto:josefspr@hotmail.com" target="_top">josefspr@hotmail.com</a> and we will clarify your request.</p>
+                <p>
+                  This database was prepared by the The Center for the Study of Cultures of Place in Jewish
+                  Modernity (hereinafter: “Daat Hamakom”).
+                </p>
+                <p>
+                  The founder of the program is the Israeli Centers for Research Excellence (I-CORE), an initiative
+                  designed by the Planning and Budgeting Committee (PBC) and the Government of Israel.
+                </p>
+                <p>
+                  The database is designed to map Jewish culture in modern times and is intended for study, research,
+                  review and criticism.
+                </p>
+                <p>
+                  The information in the database is presented as is. “Daat Hamakom” shall not be held liable for
+                  any damage that may be caused to anyone relying on this information, even if there is a mistake in
+                  the information, catalog or any other detail in the database.
+                </p>
+                <p>
+                  The contents of the database for which the original copyright holders are not specified, or otherwise
+                  stated, belong to “Daat Hamakom”. Any other use permitted by law, including for purposes of
+                  fair use as defined by law, may be made with this content. The license to use such content is conditional
+                  upon the designation of the “Daat Hamakom” center or the name of the author appearing
+                  next to the displayed content, while respecting the moral rights of the content creators.
+                </p>
+                <p>
+                  The right to use the contents of the database in respect of which the original copyright holders or
+                  authors are specified is in accordance with the terms of use of the original owners of the content.
+                  “Daat Hamakom” shall not bear any liability whatsoever as may be made by any person who violates
+                  the terms of use of the original rights holders.
+                </p>
+                <p>
+                  “Daat-Hamakom” did its best to locate the rights holders of the database works and to obtain their
+                  consent to the use made in this database. However, all rights holders may not have been located,
+                  despite the efforts made to do so. If you are the owner of a work that was used in the database
+                  and you did not give your consent, please contact us at <a href="mailto:josefspr@hotmail.com" target="_top">josefspr@hotmail.com</a> and we will take care of the matter.
+                </p>
+                <p>
+                  If you are harmed in any way by information in the database, or if you are aware of any material in
+                  the database that is not protected under the fair use protection as defined by law, please contact <a href="mailto:josefspr@hotmail.com" target="_top">josefspr@hotmail.com</a> and we will respond to your concern.
+                </p>
               </Tab.Pane>
               <Tab.Pane eventKey="about-credits">
                 <h4 className="modal-subtitle">Credits</h4>
@@ -108,6 +183,7 @@ export class AboutModal extends Component {
                 <p>Interactive and Graphic Designer: <a href="http://www.shual.com/" target="_blank">Mushon Zer-Aviv</a></p>
                 <p>Programmers: <a href="http://appandup.co.il" target="_blank">Tal Ben-Basat</a>, Yuval Adam.</p>
                 <p>Administrative Director: <a href="http://www.daat-hamakom.com/contact/" target="_blank">Anat Reches</a></p>
+                <p>Research Assistant and Video Tutorials: Guy Assal</p>
                 <p>
                   Advisory Team:
                   <a href="http://www.daat-hamakom.com/team/dr-amos-noy/" target="_blank">Dr. Amos Noy</a>,
@@ -149,9 +225,12 @@ export class HelpModal extends Component {
       </Modal.Header>
       <Modal.Body>
         <h4 className="modal-title">Help</h4>
-        <Tab.Container id="left-tabs-example" defaultActiveKey="help-timeline">
+        <Tab.Container id="left-tabs-example" defaultActiveKey="help-introductory">
           <div>
-            <Nav>
+            <Nav style={{display: 'none'}}>
+              <NavItem eventKey="help-introductory" className="modal-tab-nav">
+                Quick Introductory Tutorial
+              </NavItem>
               <NavItem eventKey="help-timeline" className="modal-tab-nav">
                 The map and the timeline
               </NavItem>
@@ -160,6 +239,11 @@ export class HelpModal extends Component {
               </NavItem>
             </Nav>
             <Tab.Content animation>
+              <Tab.Pane eventKey="help-introductory">
+                <h4 className="modal-subtitle">Quick Introductory Tutorial</h4>
+                <iframe width="540" height="310" src="https://www.youtube.com/embed/zafjsYLvQWg" className="modal-video" allowFullScreen>
+                </iframe>
+              </Tab.Pane>
               <Tab.Pane eventKey="help-timeline">
                 <h4 className="modal-subtitle">The map and the timeline</h4>
                 Use the pad or the mouse wheel to scroll and zoom in the map or the timeline. Hovering on an event on the map or the timeline opens the event thumbnail on the map and the event title appears above the hovered event on the timeline. The colour of the marker is according to the time epoch of the event.
